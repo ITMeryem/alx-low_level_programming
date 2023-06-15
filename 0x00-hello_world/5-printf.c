95219b7006ee11ab9b3fe93b9cc7394672f833d1
ITMeryem <meryemmeharzi2017@gmail.com>
2023-06-15 15:18:14 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints a piece of art with proper grammar
+ * and returns 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
