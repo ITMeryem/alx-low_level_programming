d0ad9011def28134955ebdad94816cef90aa66e7
ITMeryem <meryemmeharzi2017@gmail.com>
2023-06-15 15:31:34 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,10 @@
 /**
  * main - Entry point
  *
- * Description: Prints the string "Programming is like building a multilingual puzzle"
- *              with quotation marks using the puts function
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\"");
+puts("\"Programming is like building a multilingual puzzle");
 return 0;
 }
