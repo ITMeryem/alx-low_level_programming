5eab4f7b6daf4161f3410a28312d048b83292548
ITMeryem <meryemmeharzi2017@gmail.com>
2023-06-16 16:09:01 -0700

    8

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point of the program
  *
- * Description: This program prints all possible combinations of single-digit numbers.
+ * This program prints all possible combinations of single-digit numbers.
  *
  * Return: Always 0 (Success)
  */
